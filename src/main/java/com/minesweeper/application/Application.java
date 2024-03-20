e2568e1481532974f24161f86a474b2ea0bded5c
TikhonSakharov <andreev.tihon@gmail.com>
2024-03-20 20:38:26 +0300

    Created basic spring boot application.

---
+++ src/main/java/com/minesweeper/application/Application.java
@@ -0,0 +1,18 @@
+package main.java.com.minesweeper.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Main spring boot application.
+ */
+@Slf4j
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+        log.info("Application run with args: ", args.toString());
+    }
+}
